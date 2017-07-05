d7606dc399649e0e8ab80de3f7fad7c8e470749a
c.chhai <c.chhai@gl-f.com>
2017-07-05 08:27:07 +0700

    Android RecyclerView Kotlin

---
+++ app/src/main/java/com/chhaichivon/androidrecyclerviewkotlin/Product.kt
@@ -0,0 +1,14 @@
+package com.chhaichivon.androidrecyclerviewkotlin
+
+/**
+ * AUTHOR : CHHAI CHIVON
+ * EMAIL  : chhaichivon1995@gmail.com
+ * DATE   : 7/5/2017
+ * TIME   : 8:15 AM
+ */
+class Product {
+    private var name:String? = null
+    private var price :String? = null
+    private var description :String? = null
+
+}
---
+++ app/src/main/java/com/chhaichivon/androidrecyclerviewkotlin/ProductAdapter.kt
@@ -0,0 +1,10 @@
+package com.chhaichivon.androidrecyclerviewkotlin
+
+/**
+ * AUTHOR : CHHAI CHIVON
+ * EMAIL  : chhaichivon1995@gmail.com
+ * DATE   : 7/5/2017
+ * TIME   : 8:19 AM
+ */
+class ProductAdapter {
+}
